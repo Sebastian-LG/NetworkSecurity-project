1dd42724acb7e6bf701c404636c17a32126ad823
Sebastian Leon <joan2613@gmail.com>
2026-01-22 17:57:11 -0500

    Data validation schema definition

--- network_security/entity/config_entity.py
+++ network_security/entity/config_entity.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import os
 from network_security.constants import training_pipeline
 
+# General constants
 class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
         timestamp = timestamp.strftime("%d_%m_%Y_%H_%M_%S")
@@ -9,8 +10,9 @@ class TrainingPipelineConfig:
         self.pipeline_name = training_pipeline.PIPELINE_NAME
         self.artifact_name = training_pipeline.ARTIFACT_DIR
         self.artifact_dir = os.path.join(self.artifact_name,self.timestamp)
+        self.schema = training_pipeline.SCHEMA_FILE_PATH
         
-
+# Data ingestion constants
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_ingestion_dir:str = os.path.join(
@@ -33,4 +35,37 @@ class DataIngestionConfig:
         self.collection_name:str = training_pipeline.DATA_INGESTION_COLLECTION_NAME,
         self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
 
+# Data Validation constants
+class DataValidationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        
+        self.data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir,
+                                                     training_pipeline.DATA_VALIDATION_DIR_NAME)
+        
+        self.valid_data_dir: str = os.path.join(self.data_validation_dir,
+                                                training_pipeline.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir: str = os.path.join(self.data_validation_dir,
+                                                training_pipeline.DATA_VALIDATION_INVALID_DIR)
+        
+
+        self.valid_train_file_path: str = os.path.join(self.valid_data_dir,
+                                                training_pipeline.TRAIN_FILE_NAME)
+        self.valid_test_file_path: str = os.path.join(self.valid_data_dir,
+                                                      training_pipeline.TEST_FILE_NAME)
+
+        self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir,
+                                                training_pipeline.TRAIN_FILE_NAME)
+        self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir,
+                                                        training_pipeline.TEST_FILE_NAME)
+        
+        self.data_drift_report_file_path: str = os.path.join(
+            
+            self.data_validation_dir,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME
+        )
+
+
+        
+    
 
---
+++ network_security/utils/main_utils/utils.py
@@ -0,0 +1,19 @@
+import yaml
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+
+import os,sys
+import numpy as np
+import dill
+import pickle
+
+
+
+
+def read_yaml_file(file_path:str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
